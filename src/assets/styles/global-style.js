fdc947fe66490ef83bad889d8b92cf1fabb491bc
Calebe Rodrigues <calebebrodrigues@gmail.com>
2022-01-15 23:10:51 -0300

    Correção de bug no build

--- src/assets/styles/global-style.js
+++ src/assets/styles/global-style.js
@@ -1,5 +1,13 @@
 import { createGlobalStyle } from 'styled-components';
 
+/*
+  Cores tema:
+    Dark Purple: #260f26
+    Russian Violet: #251f47
+    Purple Navy: #404e7c
+    Shiny Shamrock: #71b48d
+    Eton Blue: #86cb92
+*/
 
 export default createGlobalStyle`
   * {
